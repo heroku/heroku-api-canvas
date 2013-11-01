b1ba5fe304ba2f24b9f8de090505e5bbfc473a42
geemus <geemus@gmail.com>
2013-11-01 15:17:33 -0500

    remove index view

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -22,9 +22,6 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 class CanvasController < ApplicationController
-  # GET /canvas
-  def index
-  end
 
   # POST /canvas
   def post
